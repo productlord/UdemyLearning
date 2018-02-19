a18d4339930ef3d698f77dbba4171d9bfd6c6a03
Shiva Badruswamy <sbadrus@digitalmobileforce.com>
2018-02-19 00:10:55 -0800

    first commit

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"math")
+	
+
+func fib(x uint64) uint64 {
+	if x == 0 {
+		return 0
+	}
+	
+	if x < 3 {
+		return x - 1
+	}
+
+	return fib(x-1) + fib(x-2)
+
+}
 
 func main() {
-	for i := 0; i < 200; i++ {
-
-		fmt.Printf("%d \t %b \t %X \t %q \n", i, i, i, i)
+	
+	for j:= 1; j < 51; j++ {
+		fmt.Println(fib(uint64(j))
 	}
 	
 }
